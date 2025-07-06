4822a07bdb4ba7d917cf791d1de44b19a3262661
qiuzhihao <2576729564@qq.com>
2025-07-06 21:28:34 +0800

    update

---
+++ aliyun.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+import os
+import sys
+import jsonlines
+from typing import List
+
+from alibabacloud_ocr_api20210707.client import Client as ocr_api20210707Client
+from alibabacloud_credentials.client import Client as CredentialClient
+from alibabacloud_tea_openapi import models as open_api_models
+from alibabacloud_darabonba_stream.client import Client as StreamClient
+from alibabacloud_ocr_api20210707 import models as ocr_api_20210707_models
+from alibabacloud_tea_util import models as util_models
+from alibabacloud_tea_util.client import Client as UtilClient
+
+
+class OCR:
+    def __init__(self, outdir):
+        os.makedirs(outdir, exist_ok=True)
+        result_path = f"{outdir}/result.jsonl"
+        self.dres = {}
+        if os.path.exists(result_path):
+            with jsonlines.open(result_path, "r") as rf:
+                for item in rf:
+                    image_path = item["image_path"]
+                    self.dres[image_path] = item
+        self.result_file = jsonlines.open(result_path, "w")
+        for _, item in self.dres.items():
+            self.result_file.write(item)
+
+        self.err_file = open(f"{outdir}/err.log", "w")
+        self.client = self.create_client()
+
+    def create_client(self) -> ocr_api20210707Client:
+        """
+        使用凭据初始化账号Client
+        @return: Client
+        @throws Exception
+        """
+        # 工程代码建议使用更安全的无AK方式，凭据配置方式请参见：https://help.aliyun.com/document_detail/378659.html。
+        # credential = CredentialClient()
+        # config = open_api_models.Config(
+        #     credential=credential
+        # )
+        config = open_api_models.Config(
+            # 您的AccessKey ID,
+            access_key_id="LTAI5t6ajBimLh4QJZZ2p9Av",
+            # 您的AccessKey Secret,
+            access_key_secret="j6AkLfueoOZkuubIpYObCVujbzhHi3"
+        )
+        # Endpoint 请参考 https://api.aliyun.com/product/ocr-api
+        config.endpoint = f'ocr-api.cn-hangzhou.aliyuncs.com'
+        return ocr_api20210707Client(config)
+
+    def parse(self, idx, path) -> None:
+        if path in self.dres:
+            return self.dres[path]
+        # 需要安装额外的依赖库，直接点击下载完整工程即可看到所有依赖。
+        body_stream = StreamClient.read_from_file_path(path)
+        recognize_all_text_request = ocr_api_20210707_models.RecognizeAllTextRequest(
+            body=body_stream,
+            type='General'
+        )
+        runtime = util_models.RuntimeOptions()
+        try:
+            res = self.client.recognize_all_text_with_options(recognize_all_text_request, runtime)
+            res = res.to_map()
+            res["image_path"] = path
+            self.result_file.write(res)
+        except Exception as error:
+            # 错误 message
+            self.err_file(f"msg={error.message}, address={error.data.get('Recommend')}")
+            UtilClient.assert_as_string(error.message)
+            res = None
+        return res
+
--- extractor.py
+++ extractor.py
@@ -2,34 +2,27 @@ import os
 import pdfplumber
 import pandas as pd
 import argparse
-import pytesseract
+import ipdb
 from tqdm import tqdm
 from multiprocessing import freeze_support, Pool
-from PIL import Image
-# import easyocr
-pytesseract.pytesseract.tesseract_cmd = "D:\\tesseract-ocr\\tesseract.exe"
-def read_picture(path):
-    image = Image.open(path)
-    # 使用pytesseract进行OCR识别
-    text = pytesseract.image_to_string(image, lang='chi_sim')
-    text = text.replace(' ', '')
-    d = {}
-    kaohao = None
-    for line in text.split('\n'):
-        if "考生号" in line:
-            lsp = line.strip().split(":")
-            kaohao = lsp[1].split('姓名')[0]
-            name = lsp[2].split('性别')[0]
-            assert kaohao not in d, kaohao
-            d[kaohao] = {'name': name}
-        elif "身份证号" in line:
-            idnum = line.strip().split(':')[1]
-            assert kaohao is not None
-            d[kaohao]['id'] = idnum
+from aliyun import OCR
+
+def read_picture(idx, path, ocr):
+    kaohao, name, idnum = None, None, None
+    res = ocr.parse(idx, path)
+
+    content = res['body']['Data']['Content']
+    for value in content.split():
+        if value.startswith("考生号"):
+            kaohao = value.replace("：", ":").split(":")[1]
+        elif value.startswith("姓名"):
+            name = value.replace("：", ":").split(":")[1]
+        elif value.startswith("身份证号"):
+            idnum = value.replace("：", ":").split(":")[1]
+
     res = []
     fname = os.path.basename(path)
-    for kaohao, item in d.items():
-        res.append([fname, kaohao, item['name'], item['id']])
+    res.append([fname, kaohao, name, idnum])
     return res
 
 def read_pdf(path):
@@ -55,16 +48,17 @@ def read_pdf(path):
         res.append([fname, kaohao, item['name'], item['id']])
     return res
 
-def read_worker(path):
+def read_worker(param):
+    idx, path, ocr = param
     try:
         if path.endswith("pdf"):
             return read_pdf(path)
         elif path.endswith("jpg") or path.endswith("png"):
-            return read_picture(path)
+            return read_picture(idx, path, ocr)
     except:
         print(f"{path} 解析失败")
     return []
-def write_excel(reslist, outpath, total_fnames):
+def write_excel(reslist, outdir, total_fnames):
     # 创建一个 DataFrame
     data1 = {
         "文件名": [],
@@ -94,9 +88,7 @@ def write_excel(reslist, outpath, total_fnames):
     df2 = pd.DataFrame(data2)
 
     # 写入 Excel 文件
-    outpath = os.path.abspath(outpath)
-    outdir = os.path.dirname(outpath)
-    os.makedirs(outdir, exist_ok=True)
+    outpath = os.path.abspath(f"{outdir}/result.xlsx")
     with pd.ExcelWriter(outpath) as writer:
         df1.to_excel(writer, index=False, sheet_name="准考证")
         df2.to_excel(writer, index=False, sheet_name="失败文件")
@@ -106,33 +98,35 @@ def write_excel(reslist, outpath, total_fnames):
 
 def main(args):
     freeze_support()  # 如果你打算打包成可执行文件，则需要此行
-    pathlist = []
+    os.makedirs(args.outdir, exist_ok=True)
+    ocr = OCR(f"{args.outdir}/logs")
+    params = []
     total_fnames = set()
-    for fname in os.listdir(args.input_dir):
+    for idx, fname in enumerate(os.listdir(args.input_dir)):
         # if not fname.endswith("jpg"):
         #     continue
-        path = f"{args.input_dir}/{fname}"
-        pathlist.append(path)
+        path = os.path.abspath(f"{args.input_dir}/{fname}")
+        params.append([idx, path, ocr])
         total_fnames.add(fname)
 
     if args.njobs > 1:
         # 创建线程池并启动任务
         with Pool(processes=args.njobs) as pool:  # 根据你的CPU核心数调整processes数量
-            results =  list(tqdm(pool.imap(read_worker, pathlist), total=len(pathlist)))
+            results =  list(tqdm(pool.imap(read_worker, params), total=len(params)))
     else:
         results = []
-        for path in pathlist:
-            result = read_worker(path)
+        for param in params:
+            result = read_worker(param)
             results.append(result)
     print("All tasks have finished.")
     # 打印结果或进行其他操作
-    write_excel(results, args.output_file, total_fnames)
+    write_excel(results, args.outdir, total_fnames)
 
 # 定义任务函数，接受参数
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("usage: python read_pdf.py /path/to/pdfdir /path/to/output.xlsx")
     parser.add_argument("input_dir", type=str, help="dir path of files")
-    parser.add_argument("output_file", type=str, help="path to output excel, for example, output.xlsx")
+    parser.add_argument("outdir", type=str, help="path to output")
     parser.add_argument("--njobs", type=int, default=16, help="num workers")
     args = parser.parse_args()
     main(args)
